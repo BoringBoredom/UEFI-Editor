6171131d80c0a33e13418882511135bc32416485
BoringBoredom <BoringBoredom>
2021-03-25 02:28:48 +0100

    update for new syntax

--- IFR-Formatter.py
+++ IFR-Formatter.py
@@ -1,6 +1,6 @@
 import re, requests
 
-current_version = 0.11
+current_version = 0.12
 
 try:
     r = requests.get("https://api.github.com/repos/BoringBoredom/IFR-Formatter/releases/latest")
@@ -11,120 +11,75 @@ try:
 except:
     pass
 
-total_oneof_settings = 0
-total_numeric_settings = 0
-total_string_settings = 0
-total_checkbox_settings = 0
-total_unknown_settings = 0
-total_shared_offsets = 0
+total_one_of = 0
+total_numeric = 0
+total_string = 0
+total_checkbox = 0
 has_options = False
-offsets = {}
 varstores = {}
 
-def check_for_unknown_setting(string):
-    global total_unknown_settings
-    if string == " " or string == " N/A":
-        total_unknown_settings += 1
+def check_unknown(string):
+    if string == "" or string == "N/A":
         return "?"
-    return string[1:]
+    else:
+        return string
 
-def gather_offsets(setting, offset):
-    if offsets.get(offset, False) == False:
-        offsets[offset] = []
-    offsets[offset].append(setting)
-
-def appendix(setting):
-    b.write(" " * 120 + current_category + "\n")
-    c.write(f"     {setting}\n")
+def append_category():
+    b.write(" " * 120 + current_form + "\n")
 
 a = open("a.txt", "r", errors="ignore")
 b = open("b.txt", "w", errors="ignore")
-c = open("c.txt", "w", errors="ignore")
-
-b.write("WARNING: USE A SHELL ALLOWING YOU TO SPECIFY VARSTORE OTHERWISE YOU RISK BRICKING YOUR SYSTEM DUE TO SHARED OFFSETS!\nWARNING: USE A SHELL ALLOWING YOU TO SPECIFY VARSTORE OTHERWISE YOU RISK BRICKING YOUR SYSTEM DUE TO SHARED OFFSETS!\nWARNING: USE A SHELL ALLOWING YOU TO SPECIFY VARSTORE OTHERWISE YOU RISK BRICKING YOUR SYSTEM DUE TO SHARED OFFSETS!\n\n")
 
 for line in a:
-    varstore = re.search(r"VarStoreId: (.+) \[.+ Name: (.+) {", line)
-    category = re.search(r"Form:(.+), FormId:", line)
-    oneof_setting = re.search(r"One Of:(.+), VarStoreInfo \(VarOffset/VarName\): (.+), VarStore: (.+), QuestionId:", line)
-    option = re.search(r"One Of Option:(.+), Value \(.+ bit\): (.+) {", line)
-    numeric_setting = re.search(r"Numeric:(.+), VarStoreInfo \(VarOffset/VarName\): (.+), VarStore: (.+), QuestionId: .+ Min: (.+), Max (.+), Step: (.+) {", line)
-    string_setting = re.search(r"String:(.+), VarStoreInfo \(VarOffset/VarName\): (.+), VarStore: (.+), QuestionId:", line)
-    checkbox_setting = re.search(r"Checkbox:(.+), VarStoreInfo \(VarOffset/VarName\): (.+), VarStore: (.+), QuestionId:", line)
-    end_of_options = re.search(r"End One Of", line)
-    end_of_category = re.search(r"End Form", line)
+    varstore = re.search(r"VarStoreId: (.*) \[.*Name: (.*) {", line)
+    form = re.search(r"Form: (.*), FormId", line)
+    end_form = re.search(r"End Form", line)
+    one_of = re.search(r"One Of: (.*), VarStoreInfo \(VarOffset/VarName\): (.*), VarStore: (.*), QuestionId.*Size: (.*), Min", line)
+    one_of_option = re.search(r"One Of Option: (.*), Value \(.*bit\): (.*) {", line)
+    end_one_of = re.search(r"End One Of", line)
+    numeric = re.search(r"Numeric: (.*), VarStoreInfo \(VarOffset/VarName\): (.*), VarStore: (.*), QuestionId.*Size: (.*), Min: (.*), Max (.*), Step: (.*) {", line)
+    string = re.search(r"String: (.*), VarStoreInfo \(VarOffset/VarName\): (.*), VarStore: (.*), QuestionId", line)
+    checkbox = re.search(r"Checkbox: (.*), VarStoreInfo \(VarOffset/VarName\): (.*), VarStore: (.*), QuestionId", line)
+
     if varstore:
         varstores[varstore.group(1)] = varstore.group(2)
-    elif category:
-        current_category = check_for_unknown_setting(category.group(1))
-        b.write(f"{current_category}\n\n")
-        c.write(f"{current_category}\n\n")
-    elif oneof_setting:
-        current_oneof_setting = check_for_unknown_setting(oneof_setting.group(1))
-        gather_offsets(current_oneof_setting, oneof_setting.group(2))
-        b.write(f"     {current_oneof_setting} | VarOffset: {oneof_setting.group(2)}, VarStore: {varstores[oneof_setting.group(3)]} ({oneof_setting.group(3)})\n")
-        c.write(f"     {current_oneof_setting}\n")
-        total_oneof_settings += 1
+    elif form:
+        current_form = check_unknown(form.group(1))
+        b.write(f"{current_form}\n\n")
+    elif end_form:
+        b.write("\n")
+    elif one_of:
+        current_one_of = check_unknown(one_of.group(1))
+        b.write(f"     {current_one_of} | VarStore: {varstores[one_of.group(3)]}, VarOffset: {one_of.group(2)}, Size: {hex(int(one_of.group(4)))}\n")
+        total_one_of += 1
         has_options = True
-    elif option:
-        current_option = check_for_unknown_setting(option.group(1))
-        b.write(f"          {current_option}: {option.group(2)}\n")
+    elif one_of_option:
+        current_one_of_option = check_unknown(one_of_option.group(1))
+        b.write(f"          {current_one_of_option}: {one_of_option.group(2)}\n")
         if has_options == False:
             b.write("\n")
-    elif numeric_setting:
-        current_numeric_setting = check_for_unknown_setting(numeric_setting.group(1))
-        gather_offsets(current_numeric_setting, numeric_setting.group(2))
-        b.write(f"     {current_numeric_setting} | VarOffset: {numeric_setting.group(2)}, VarStore: {varstores[numeric_setting.group(3)]} ({numeric_setting.group(3)})\n          Min: {numeric_setting.group(4)}, Max: {numeric_setting.group(5)}, Step: {numeric_setting.group(6)}\n")
-        appendix(current_numeric_setting)
-        total_numeric_settings += 1
-    elif string_setting:
-        current_string_setting = check_for_unknown_setting(string_setting.group(1))
-        gather_offsets(current_string_setting, string_setting.group(2))
-        b.write(f"     {current_string_setting} | VarOffset: {string_setting.group(2)}, Varstore: {varstores[string_setting.group(3)]} ({string_setting.group(3)})\n")
-        appendix(current_string_setting)
-        total_string_settings += 1
-    elif checkbox_setting:
-        current_checkbox_setting = check_for_unknown_setting(checkbox_setting.group(1))
-        gather_offsets(current_checkbox_setting, checkbox_setting.group(2))
-        b.write(f"     {current_checkbox_setting} | VarOffset: {checkbox_setting.group(2)}, Varstore: {varstores[checkbox_setting.group(3)]} ({checkbox_setting.group(3)})\n")
-        appendix(current_checkbox_setting)
-        total_checkbox_settings += 1
-    elif end_of_options:
-        b.write(" " * 120 + current_category + "\n")
+    elif end_one_of:
+        append_category()
         has_options = False
-    elif end_of_category:
-        b.write("\n")
-        c.write("\n")
+    elif numeric:
+        current_numeric = check_unknown(numeric.group(1))
+        b.write(f"     {current_numeric} | VarStore: {varstores[numeric.group(3)]}, VarOffset: {numeric.group(2)}, Size: {hex(int(numeric.group(4)))}\n          Min: {numeric.group(5)}, Max: {numeric.group(6)}, Step: {numeric.group(7)}\n")
+        append_category()
+        total_numeric += 1
+    elif string:
+        current_string = check_unknown(string.group(1))
+        b.write(f"     {current_string} | Varstore: {varstores[string.group(3)]}, VarOffset: {string.group(2)}\n")
+        append_category()
+        total_string += 1
+    elif checkbox:
+        current_checkbox = check_unknown(checkbox.group(1))
+        b.write(f"     {current_checkbox} | Varstore: {varstores[checkbox.group(3)]}, VarOffset: {checkbox.group(2)}\n")
+        append_category()
+        total_checkbox += 1
 
-b.write(f"\nShared offsets:\n\n")
+total_settings = total_one_of + total_numeric + total_string + total_checkbox
 
-for offset, settings in offsets.items():
-    settings = set(settings)
-    if len(settings) > 1:
-        total_shared_offsets += 1
-        b.write(f"{offset}: ")
-        entry_count = 1
-        are_unknown = False
-        temp_settings = []
-        for setting in settings:
-            if setting != "?":
-                temp_settings.append(setting)
-            else:
-                are_unknown = True
-        for entry in temp_settings:
-            b.write(entry)
-            if entry_count < len(temp_settings):
-                b.write(" | ")
-                entry_count += 1
-        if are_unknown == True:
-            b.write(f" | 1 or more unknown settings")
-        b.write("\n")
-
-total_settings = total_oneof_settings + total_numeric_settings + total_string_settings + total_checkbox_settings
-
-b.write(f"\n\nTotal settings: {total_settings}\nTotal one of settings: {total_oneof_settings}\nTotal numeric settings: {total_numeric_settings}\nTotal string settings: {total_string_settings}\nTotal checkbox settings: {total_checkbox_settings}\nTotal unknown settings: {total_unknown_settings}\nTotal shared offsets: {total_shared_offsets}\n")
-c.write(f"\n\nTotal settings: {total_settings}\nTotal one of settings: {total_oneof_settings}\nTotal numeric settings: {total_numeric_settings}\nTotal string settings: {total_string_settings}\nTotal checkbox settings: {total_checkbox_settings}\nTotal unknown settings: {total_unknown_settings}\nTotal shared offsets: {total_shared_offsets}\n")
+b.write(f"\n\nTotal settings: {total_settings}\nTotal One Of settings: {total_one_of}\nTotal Numeric settings: {total_numeric}\nTotal String settings: {total_string}\nTotal Checkbox settings: {total_checkbox}")
 
 a.close()
 b.close()
-c.close()
--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 ## Prerequisites
 https://github.com/LongSoft/UEFITool/releases  
 https://github.com/LongSoft/Universal-IFR-Extractor/releases  
-https://github.com/datasone/grub-mod-setup_var/issues/5#issuecomment-612967491 (variable name = VarStore name)  
-https://github.com/XDleader555/grub_setup_var/releases + https://github.com/XDleader555/grub_setup_var/issues/4#issuecomment-615953345  
+https://github.com/datasone/grub-mod-setup_var/releases  
 
 ## Preparation
-Download [datasone's](https://github.com/datasone/grub-mod-setup_var/files/4470388/modGRUBShellCustomVarName.zip) or [XDleader555's](https://github.com/XDleader555/grub_setup_var/releases) modded shell and rename it to ***BOOTX64.EFI***.
+Download [datasone's modded shell](https://github.com/datasone/grub-mod-setup_var/releases) and rename it to ***BOOTX64.EFI***.
 
 Format a USB drive as FAT32 and put ***BOOTX64.EFI*** in ***USB:\EFI\BOOT\\*** (create the folders ***EFI*** and ***BOOT*** manually). The final path of the shell will be ***USB:\EFI\BOOT\BOOTX64.EFI***.
 
@@ -15,8 +14,6 @@ Download your **current** BIOS version from the motherboard vendor's site. Offse
 Drag and drop it into [UEFITool](https://github.com/LongSoft/UEFITool/releases). Press CTRL + F and perform a ***Text*** search for any setting. Double-click the reference to the section at the bottom, right-click the section (usually under ***Setup***) and press ***Extract as is...***.
 
 Convert the extracted file with [IRFExtractor](https://github.com/LongSoft/Universal-IFR-Extractor/releases). Rename the output to ***a.txt***, move it to the folder containing [this script](https://github.com/BoringBoredom/IFR-Formatter/releases) and run it.
-
-Open ***b.txt***, search for the settings you want to change and write down the ***VarOffsets***, ***Values*** and ***VarStore*** names.
 
 Disable Secure Boot and boot from the USB drive in UEFI mode (CSM disabled). You can enable CSM again after you're done.
 
@@ -33,26 +30,17 @@ End One Of {29 02}
 ```
 ### Inside *b.txt*
 ```
-High Precision Timer | VarOffset: 0x20, VarStore: PchSetup (0x7)
+High Precision Timer | VarStore: PchSetup, VarOffset: 0x20, Size: 0x1
      Disabled: 0x0
      Enabled: 0x1 (default)
 ```
-### Using datasone’s shell
+### [Syntax](https://github.com/datasone/grub-mod-setup_var#setup_var_cv) (<- read this)
 In the GRUB command interface, type:  
-***setup_var VarOffset Value VarStoreName*** (e.g. ***setup_var 0x20 0x0 PchSetup***)
+***setup_var_cv VarStore VarOffset Size Value *** (e.g. ***setup_var_cv PchSetup 0x20 0x1 0x0***)
 
-To obtain the current value without changing it, replace ***Value*** with ***read*** (e.g. ***setup_var 0x20 read PchSetup***).
-
-If you encounter [this issue](https://github.com/datasone/grub-mod-setup_var/blob/master/README.md#the-problem), use ***setup_var_3***.
+To obtain the current value without changing it, omit ***Value*** (e.g. ***setup_var_cv PchSetup 0x20 0x1***).
 
 To exit and reboot, type:  
 ***reboot***
 
-### Using XDleader555's shell
-In the GRUB command interface, type:  
-***setup_var VarStoreName VarOffset Value*** (e.g. ***setup_var PchSetup 0x20 0x0***)
-
-To obtain the current value without changing it, omit ***Value*** (e.g. ***setup_var PchSetup 0x20***).
-
-To exit and reboot, type:  
-***reboot***
+If something unexpected happens, force shutdown and reset CMOS.

a834a2dc16c2b222b8a816dc5d280ecd26186b8a
BoringBoredom <16615327+BoringBoredom@users.noreply.github.com>
2022-12-11 05:53:46 +0200

    add LoadingOverlay

--- src/components/FileUploads/FileUploads.tsx
+++ src/components/FileUploads/FileUploads.tsx
@@ -3,7 +3,15 @@ import { Updater } from "use-immer";
 import setupSctPng from "../images/setupSct.png";
 import amitseSctPng from "../images/amitseSct.png";
 import setupdataBinPng from "../images/setupdataBin.png";
-import { Tooltip, FileInput, Image, Stack, Group, Button } from "@mantine/core";
+import {
+  Tooltip,
+  FileInput,
+  Image,
+  Stack,
+  Group,
+  Button,
+  LoadingOverlay,
+} from "@mantine/core";
 import { IconDownload, IconUpload } from "@tabler/icons";
 import { Data, parseData } from "../scripts";
 
@@ -64,130 +72,140 @@ export function FileUploads({ files, setFiles, setData }: FileUploadsProps) {
   }, [files, setFiles, setData]);
 
   return (
-    <Stack>
-      <Group grow>
-        <Button
-          variant="default"
-          size="lg"
-          component="a"
-          href="https://github.com/LongSoft/UEFITool/releases"
-          target="_blank"
-          leftIcon={<IconDownload />}
+    <>
+      <LoadingOverlay
+        visible={Object.values(files).every(
+          (fileContainer) => fileContainer.file && !fileContainer.isWrongFile
+        )}
+        loaderProps={{ size: "xl" }}
+      />
+      <Stack>
+        <Group grow>
+          <Button
+            variant="default"
+            size="lg"
+            component="a"
+            href="https://github.com/LongSoft/UEFITool/releases"
+            target="_blank"
+            leftIcon={<IconDownload />}
+          >
+            {`UEFITool`}
+          </Button>
+
+          <Button
+            variant="default"
+            size="lg"
+            component="a"
+            href="https://github.com/LongSoft/IFRExtractor-RS/suites/9555751857/artifacts/454700873"
+            target="_blank"
+            leftIcon={<IconDownload />}
+          >
+            {`IFRExtractor-RS`}
+          </Button>
+        </Group>
+
+        <Tooltip label={<Image src={setupSctPng} />}>
+          <FileInput
+            icon={<IconUpload />}
+            size="lg"
+            placeholder="Setup SCT"
+            accept=".sct"
+            value={files.setupSctContainer.file}
+            error={files.setupSctContainer.isWrongFile}
+            onChange={(file) => {
+              if (file) {
+                const name = file.name.toLowerCase();
+
+                setFiles((draft) => {
+                  draft.setupSctContainer = {
+                    file,
+                    isWrongFile: !(
+                      name.includes("setup") && name.endsWith(".sct")
+                    ),
+                  };
+                });
+              }
+            }}
+          />
+        </Tooltip>
+
+        <Tooltip
+          label={`ifrextractor.exe "Section_PE32_image_Setup.sct" verbose`}
         >
-          {`UEFITool`}
-        </Button>
+          <FileInput
+            icon={<IconUpload />}
+            size="lg"
+            placeholder="IFR Extractor output TXT"
+            accept=".txt"
+            value={files.setupTxtContainer.file}
+            error={files.setupTxtContainer.isWrongFile}
+            onChange={(file) => {
+              if (file) {
+                const name = file.name.toLowerCase();
 
-        <Button
-          variant="default"
-          size="lg"
-          component="a"
-          href="https://github.com/LongSoft/IFRExtractor-RS/suites/9555751857/artifacts/454700873"
-          target="_blank"
-          leftIcon={<IconDownload />}
-        >
-          {`IFRExtractor-RS`}
-        </Button>
-      </Group>
+                setFiles((draft) => {
+                  draft.setupTxtContainer = {
+                    file,
+                    isWrongFile: !(
+                      name.includes("ifr") && name.endsWith(".txt")
+                    ),
+                  };
+                });
+              }
+            }}
+          />
+        </Tooltip>
 
-      <Tooltip label={<Image src={setupSctPng} />}>
-        <FileInput
-          icon={<IconUpload />}
-          size="lg"
-          placeholder="Setup SCT"
-          accept=".sct"
-          value={files.setupSctContainer.file}
-          error={files.setupSctContainer.isWrongFile}
-          onChange={(file) => {
-            if (file) {
-              const name = file.name.toLowerCase();
+        <Tooltip label={<Image src={amitseSctPng} />}>
+          <FileInput
+            icon={<IconUpload />}
+            size="lg"
+            placeholder="AMITSE SCT"
+            accept=".sct"
+            value={files.amitseSctContainer.file}
+            error={files.amitseSctContainer.isWrongFile}
+            onChange={(file) => {
+              if (file) {
+                const name = file.name.toLowerCase();
 
-              setFiles((draft) => {
-                draft.setupSctContainer = {
-                  file,
-                  isWrongFile: !(
-                    name.includes("setup") && name.endsWith(".sct")
-                  ),
-                };
-              });
-            }
-          }}
-        />
-      </Tooltip>
+                setFiles((draft) => {
+                  draft.amitseSctContainer = {
+                    file,
+                    isWrongFile: !(
+                      name.includes("amitse") && name.endsWith(".sct")
+                    ),
+                  };
+                });
+              }
+            }}
+          />
+        </Tooltip>
 
-      <Tooltip
-        label={`ifrextractor.exe "Section_PE32_image_Setup.sct" verbose`}
-      >
-        <FileInput
-          icon={<IconUpload />}
-          size="lg"
-          placeholder="IFR Extractor output TXT"
-          accept=".txt"
-          value={files.setupTxtContainer.file}
-          error={files.setupTxtContainer.isWrongFile}
-          onChange={(file) => {
-            if (file) {
-              const name = file.name.toLowerCase();
+        <Tooltip label={<Image src={setupdataBinPng} />}>
+          <FileInput
+            icon={<IconUpload />}
+            size="lg"
+            placeholder="Setupdata BIN"
+            accept=".bin"
+            value={files.setupdataBin.file}
+            error={files.setupdataBin.isWrongFile}
+            onChange={(file) => {
+              if (file) {
+                const name = file.name.toLowerCase();
 
-              setFiles((draft) => {
-                draft.setupTxtContainer = {
-                  file,
-                  isWrongFile: !(name.includes("ifr") && name.endsWith(".txt")),
-                };
-              });
-            }
-          }}
-        />
-      </Tooltip>
-
-      <Tooltip label={<Image src={amitseSctPng} />}>
-        <FileInput
-          icon={<IconUpload />}
-          size="lg"
-          placeholder="AMITSE SCT"
-          accept=".sct"
-          value={files.amitseSctContainer.file}
-          error={files.amitseSctContainer.isWrongFile}
-          onChange={(file) => {
-            if (file) {
-              const name = file.name.toLowerCase();
-
-              setFiles((draft) => {
-                draft.amitseSctContainer = {
-                  file,
-                  isWrongFile: !(
-                    name.includes("amitse") && name.endsWith(".sct")
-                  ),
-                };
-              });
-            }
-          }}
-        />
-      </Tooltip>
-
-      <Tooltip label={<Image src={setupdataBinPng} />}>
-        <FileInput
-          icon={<IconUpload />}
-          size="lg"
-          placeholder="Setupdata BIN"
-          accept=".bin"
-          value={files.setupdataBin.file}
-          error={files.setupdataBin.isWrongFile}
-          onChange={(file) => {
-            if (file) {
-              const name = file.name.toLowerCase();
-
-              setFiles((draft) => {
-                draft.setupdataBin = {
-                  file,
-                  isWrongFile: !(
-                    name.includes("setupdata") && name.endsWith(".bin")
-                  ),
-                };
-              });
-            }
-          }}
-        />
-      </Tooltip>
-    </Stack>
+                setFiles((draft) => {
+                  draft.setupdataBin = {
+                    file,
+                    isWrongFile: !(
+                      name.includes("setupdata") && name.endsWith(".bin")
+                    ),
+                  };
+                });
+              }
+            }}
+          />
+        </Tooltip>
+      </Stack>
+    </>
   );
 }
